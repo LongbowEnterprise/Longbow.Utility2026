dd86c6bf27264f1e879198fa54707a25ddd395c0
Argo Zhang <argo@live.ca>
2025-11-13 08:07:15 +0800

    refactor: 合并注入方法

--- src/Longbow.Logging/Extensions/FileLoggerFactoryExtensions.cs
+++ src/Longbow.Logging/Extensions/FileLoggerFactoryExtensions.cs
@@ -4,7 +4,9 @@
 
 using Longbow.Logging;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
 using Microsoft.Extensions.Options;
+using System.Collections.Specialized;
 using System.Runtime.InteropServices;
 
 namespace Microsoft.Extensions.DependencyInjection;
@@ -21,16 +23,26 @@ public static class FileLoggerFactoryExtensions
     /// <param name="configure"></param>
     public static ILoggingBuilder AddFileLogger(this ILoggingBuilder builder, Action<FileLoggerOptions>? configure = null)
     {
-        builder.Services.AddOptions();
-        builder.Services.AddSingleton<IOptionsChangeTokenSource<FileLoggerOptions>, ConfigurationChangeTokenSource<FileLoggerOptions>>();
-        builder.Services.AddSingleton<IConfigureOptions<FileLoggerOptions>, ConfigureOptions<FileLoggerOptions>>();
-
         builder.Services.AddSingleton<ILoggerProvider, FileLoggerProvider>();
+        builder.Services.AddSingleton<IConfigureOptions<FileLoggerOptions>, LoggerProviderConfigureOptions<FileLoggerOptions, FileLoggerProvider>>();
+        builder.Services.AddSingleton<IOptionsChangeTokenSource<FileLoggerOptions>, LoggerProviderOptionsChangeTokenSource<FileLoggerOptions, FileLoggerProvider>>();
+
         builder.Services.Configure<FileLoggerOptions>(op =>
         {
             op.FileName = GetOSPlatformPath(op.FileName);
             configure?.Invoke(op);
         });
+        return builder;
+    }
+
+    /// <summary>
+    /// 添加文件日志功能到服务容器中
+    /// </summary>
+    /// <param name="builder">The <see cref="ILoggingBuilder"/> to use.</param>
+    /// <param name="action">日志操作类回调方法，使用者在此回调中将异常记录到数据库中</param>
+    public static ILoggingBuilder AddDBLogger(this ILoggingBuilder builder, Action<IServiceProvider, EventId, Exception?, NameValueCollection> action)
+    {
+        builder.Services.AddSingleton<ILoggerProvider>(provider => new DBLoggerProvider(provider, action));
         return builder;
     }
 
