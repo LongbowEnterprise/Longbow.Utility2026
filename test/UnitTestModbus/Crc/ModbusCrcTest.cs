b08e02913e95038a985937f8197bf8e71d34b168
Argo Zhang <argo@live.ca>
2026-07-28 09:26:30 +0800

    test: 增加单元测试

--- test/UnitTestModbus/Crc/ModbusCrcTest.cs
+++ test/UnitTestModbus/Crc/ModbusCrcTest.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
+// Copyright (c) Argo Zhang (argo@live.ca). All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 // Website: https://github.com/LongbowExtensions/
 
@@ -17,6 +17,24 @@ public class ModbusCrcTest
         var crc = ModbusCrc16.Compute(data);
         Assert.Equal("BAC9", crc.ToString("X4"));
         Assert.Equal("01060000017FC9BA", HexConverter.ToString(ModbusCrc16.Append(data), ""));
+
+        data = [0x01, 0x03, 0x00, 0x12, 0x00, 0x02];
+        crc = ModbusCrc16.Compute(data);
+        Assert.Equal("0E64", crc.ToString("X4"));
+        Assert.Equal("010300120002640E", HexConverter.ToString(ModbusCrc16.Append(data), ""));
+    }
+
+    [Fact]
+    public void AppendMemory_Ok()
+    {
+        Memory<byte> buffer = new byte[16];
+        byte[] data = [0x01, 0x03, 0x00, 0x12, 0x00, 0x02];
+        data.CopyTo(buffer);
+
+        var len = ModbusCrc16.Append(buffer, 6);
+
+        Assert.Equal(8, len);
+        Assert.Equal("010300120002640E", HexConverter.ToString(buffer[..len].Span, ""));
     }
 
     [Fact]
